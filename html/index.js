d5d41d7d281227a7cf4f2f1992f4b7249874ab33
Harsh123k <103120793+Harsh123k@users.noreply.github.com>
2024-02-13 00:10:03 +0530

    Add files via upload

---
+++ html/index.js
@@ -0,0 +1,50 @@
+document.getElementById('button1').onclick = function () {
+    document.getElementById('step1').style.display = 'none';
+    document.getElementById('step2').style.display = 'block';
+  };
+  
+  document.getElementById('button2').onclick = function () {
+    document.getElementById('step2').style.display = 'none';
+    document.getElementById('step3').style.display = 'block';
+  };
+  
+  document.getElementById('button3').onclick = function () {
+    document.getElementById('step3').style.display = 'none';
+    document.getElementById('step4').style.display = 'block';
+  };
+  
+  document.getElementById('button41').onclick = function () {
+    document.getElementById('step4').style.display = 'none';
+    document.getElementById('gifSection').style.display = 'block';
+  };
+  
+  document.getElementById('button42').onclick = function () {
+    document.getElementById('step4').style.display = 'none';
+    document.getElementById('step5').style.display = 'block';
+  };
+  
+  document.getElementById('button43').onclick = function () {
+    document.getElementById('step4').style.display = 'none';
+    document.getElementById('step7').style.display = 'block';
+  };
+  
+  document.getElementById('button51').onclick = function () {
+    document.getElementById('step5').style.display = 'none';
+    document.getElementById('step6').style.display = 'block';
+  };
+  
+  document.getElementById('button52').onclick = function () {
+    document.getElementById('step6').style.display = 'none';
+    document.getElementById('gifSection').style.display = 'block';
+  };
+  
+  document.getElementById('button61').onclick = function () {
+    document.getElementById('step7').style.display = 'none';
+    document.getElementById('step8').style.display = 'block';
+  };
+  
+  document.getElementById('button62').onclick = function () {
+    document.getElementById('step8').style.display = 'none';
+    document.getElementById('gifSection').style.display = 'block';
+  };
+  
